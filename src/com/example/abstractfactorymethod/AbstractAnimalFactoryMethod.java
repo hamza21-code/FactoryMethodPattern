c60392766151f19edf8b33465ec253a5457ae11d
hamza21-code <hamza21code@gmail.com>
2022-06-20 15:21:59 +0600

    Added comments as virtual constructor

--- src/com/example/abstractfactorymethod/AbstractAnimalFactoryMethod.java
+++ src/com/example/abstractfactorymethod/AbstractAnimalFactoryMethod.java
@@ -2,7 +2,7 @@ package com.example.abstractfactorymethod;
 
 import com.example.creator.Animal;
 
-public abstract  class AbstractAnimalFactoryMethod {
+public abstract  class AbstractAnimalFactoryMethod { // Factory Method patterns call Virtual Constructor. Where subclasses are responsible to create Objects Or Instance of Class.
 
     protected abstract Animal getAnimalType(String animalType);
 }
